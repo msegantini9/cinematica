038294977b79fa555b1db581e8ed1c7011b542e6
Matheus <m.segantini9@gmail.com>
2025-04-12 22:38:50 -0300

    Realizado coneão com API OMDb

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-alert('oie!')
+const apiKey = "dc3a431a";
+let movies = [];
+
+async function getMovies(num) {
+
+    let randomNumber, url;
+
+    for (let i=0; i<num; i++) {
+        
+        randomNumber = Math.round((Math.random())*100);
+        url = `https://www.omdbapi.com/?i=tt000000${randomNumber}&apikey=${apiKey}`
+
+        try {
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const movie = await response.json();
+            movies.push(movie);
+
+        } catch (error) {
+            console.error(error.message);
+        }
+
+    }
+
+    console.log(movies)
+}
+
